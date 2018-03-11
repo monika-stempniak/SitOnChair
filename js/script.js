0c36fb5c990231aeb1935f9679aec7aa1f21700f
monika <mstempniak@gmail.com>
2018-03-11 12:00:41 +0100

    add hiding info-box func

--- js/script.js
+++ js/script.js
@@ -23,5 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 
+  // ----- hiding info-box text ------
+  // variables
+  var infoBoxes = document.querySelectorAll('.info-box');
+
+  // mouse events
+  infoBoxes.forEach(function(box) {
+    box.addEventListener('mouseover', function() {
+      var infoBoxBlock = this.querySelector('.info-box-cnt');
+      var infoBoxImage = this.querySelector('.info-box-img');
+      infoBoxBlock.classList.add('hide'); // .hide hides blocks
+      infoBoxImage.classList.add('enlarge'); // .enlarge makes images slightly bigger
+    });
+    box.addEventListener('mouseout', function() {
+      var infoBoxBlock = this.querySelector('.info-box-cnt');
+      var infoBoxImage = this.querySelector('.info-box-img');
+      infoBoxBlock.classList.remove('hide');
+      infoBoxImage.classList.remove('enlarge');
+    });
+  });
+
+
 
 });
